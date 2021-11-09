3c6fcbd0014ada488fa1b314af92e7f0705c4928
Dariusz Okonski <darek200180@gmail.com>
2021-11-09 17:05:41 +0000

    add class GameUtils

---
+++ src/main/java/com/codecool/capture_the_flag/util/GameUtils.java
@@ -0,0 +1,80 @@
+package main.java.com.codecool.capture_the_flag.util;
+
+import main.java.com.codecool.capture_the_flag.actors.Actor;
+
+public class GameUtils {
+    /**
+     * Returns a character representing given actor
+     * Throws an IllegalArgumentException whe given invalid input
+     *
+     * @param actor
+     * @return
+     * */
+    public static char getChar(Actor actor) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Returns a vector representing given direction
+     * Throws an IllegalArgumentException when given invalid input
+     *
+     * @param dir
+     * @return
+     * */
+    public static Vector toVector(Direction dir) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Returns a direction converted from given vector
+     * Throws an IllegalArgumentException when given invalid input
+     *
+     * @param vector
+     * @return
+     * */
+    public static Direction toDirection(Vector vector) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Returns a direction that is opposite to given direction
+     * Throws an IllegalArgumentException when given invalid input
+     *
+     * @param dir
+     * @return
+     * */
+    public static Direction inverted(Direction dir) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    /**
+     * Returns the amount of steps a player has to make n order to get from pos1 to pos2
+     *
+     * @param pos1
+     * @param pos2
+     * @return
+     * */
+    public static int getDistance(Vector pos1, Vector pos2) {
+        throw new RuntimeException("Method not implemented");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
