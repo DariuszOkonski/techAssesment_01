948289814c00a62a7d7bebd73f2cc0a4c8d754d9
Dariusz Okonski <darek200180@gmail.com>
2021-11-09 16:49:46 +0000

    add class Paper

---
+++ src/com/codecool/capture_the_flag/actors/Paper.java
@@ -0,0 +1,34 @@
+package com.codecool.capture_the_flag.actors;
+
+import com.codecool.capture_the_flag.GameMap;
+
+/**
+ * Paper player class
+ * */
+public class Paper extends Player{
+
+
+    public Paper(String name, GameMap mapReference) {
+        super(name, mapReference);
+        this.letter = 'P';
+    }
+
+    @Override
+    public void onGameCycle() {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    private boolean isActorEnemy(Actor actor) {
+        throw new RuntimeException("Method not implemented");
+    }
+
+    @Override
+    public int Fight(Player otherPlayer) {
+        return 0;
+    }
+
+    @Override
+    public PlayerTeam getTeam() {
+        throw new RuntimeException("Method not implemented");
+    }
+}
