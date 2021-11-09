ba5c7aec68669f53c8ae1b5d39b1a51b0f0cecab
Dariusz Okonski <darek200180@gmail.com>
2021-11-09 14:45:12 +0000

    add class Actor and compose it into GameMap

--- src/com/codecool/capture_the_flag/GameMap.java
+++ src/com/codecool/capture_the_flag/GameMap.java
@@ -1,6 +1,19 @@
 package com.codecool.capture_the_flag;
 
+import com.codecool.capture_the_flag.actors.Actor;
+
+/**
+ * GameMap class
+ * */
 public class GameMap {
-    public GameMap(String testMap) {
+
+    /**
+     * A 2D matrix of all actor references (null if the field is empty)
+     * */
+    private final Actor[][] actorMatrix;
+
+    public GameMap(String charMatrix) {
+        String[] lines = charMatrix.split(System.lineSeparator());
+        actorMatrix = new Actor[lines.length][lines[0].length()];
     }
 }
---
+++ src/com/codecool/capture_the_flag/actors/Actor.java
@@ -0,0 +1,34 @@
+package com.codecool.capture_the_flag.actors;
+
+import com.codecool.capture_the_flag.GameMap;
+
+/**
+ * Base class for all Actors - Players and Flags
+ * */
+public abstract class Actor {
+
+    protected char letter;
+    /**
+     * Reference to Game's map
+     * */
+    protected GameMap mapReference;
+
+    public Actor(GameMap mapReference) {
+        this.mapReference = mapReference;
+    }
+
+    public GameMap getMapReference() {
+        return mapReference;
+    }
+
+    public char getLetter() {
+        return letter;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor{" +
+                "letter=" + letter +
+                '}' + getClass();
+    }
+}
