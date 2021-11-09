3dc01f72cc900542925c8fc1bd58948357d107e9
Dariusz Okonski <darek200180@gmail.com>
2021-11-09 16:04:53 +0000

    add class ActorFactory and constructor to GameMap

--- src/com/codecool/capture_the_flag/GameMap.java
+++ src/com/codecool/capture_the_flag/GameMap.java
@@ -1,6 +1,7 @@
 package com.codecool.capture_the_flag;
 
 import com.codecool.capture_the_flag.actors.Actor;
+import com.codecool.capture_the_flag.actors.ActorFactory;
 import com.codecool.capture_the_flag.actors.Flag;
 import com.codecool.capture_the_flag.actors.Player;
 
@@ -36,5 +37,52 @@ public class GameMap {
         actorMatrix = new Actor[lines.length][lines[0].length()];
         this.players = new ArrayList<>();
         this.flags = new ArrayList<>();
+
+        for (int i = 0; i < lines.length; i++) {
+            char[] chars = lines[i].toCharArray();
+            for (int y = 0; y < lines.length; y++) {
+                Actor fromChar = ActorFactory.createFromChar(chars[y], this);
+                if(fromChar instanceof Player) {
+                    players.add((Player) fromChar);
+                } else if(fromChar instanceof Flag) {
+                    flags.add((Flag) fromChar);
+                }
+                actorMatrix[i][y] = fromChar;
+            }
+
+        }
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
